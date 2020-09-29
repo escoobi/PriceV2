d8c83c0038f2b257b9a48ab05edbbc7bc8f9dca1
escoobi <gustavo_o_@hotmail.com>
2020-09-29 16:55:15 -0300

    Finalizando

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { KeyboardAvoidingView, StatusBar, FlatList, Text, StyleSheet, View, Image, TouchableOpacity, Platform } from 'react-native';
+import { KeyboardAvoidingView, StatusBar, FlatList, Text, StyleSheet, View, Modal, Image, TouchableOpacity, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
@@ -65,6 +65,10 @@ function Principal() {
   const navigation = useNavigation();
   const [empresas, setEmpresas] = useState([]);
   const [filteredEmpresas, setFilteredEmpresas] = useState([]);
+
+
+
+
   useEffect(() => {
     fetch('http://price.app.br:3000/empresa')
       .then((res) => res.json())
@@ -107,6 +111,7 @@ function Principal() {
   const [selectedDifPercDamodaran, setSelectedDifPercDamodaran] = useState(null);
   const [selectedMediaValution, setSelectedMediaValution] = useState(null);
   const [selectedRiscoMedia, setSelectedRiscoMedia] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
   const renderItem = ({ item }) => {
     const backgroundColor = item.tik === selectedTik ? '#FFF9' : '#FFF';
     return <Item tik={item.tik} logo={item.logo} onPress={() => getItem(item)} style={{ backgroundColor }} />;
@@ -120,17 +125,75 @@ function Principal() {
     setSelectedIntrisseco(item.intrisseco);
     setSelectedDamodaran(item.damodaran);
     setSelectedDifPercIntrisseco(item.difpercintrisseco);
-    setSelectedDifPercDamodaran(item.difpercdamoraran);
+    setSelectedDifPercDamodaran(item.difpercdamodaran);
     setSelectedMediaValution(item.mediavalution);
     setSelectedRiscoMedia(item.riscomedia);
-    console.log(item.logo);
+    setModalVisible(!modalVisible);
     setFilteredEmpresas([]);
 
   };
 
+
+
   return (
+
+
+
+
+
     <View style={styles.containerPesquisa}>
       <StatusBar barStyle='dark-content'></StatusBar>
+
+      <Modal
+        animationType={'slide'}
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}>
+
+        <View style={styles.containerPesquisaEmpresaResultadoShow}>
+
+          <View style={styles.containerPraRiba}>
+            <View style={styles.containerLogo}>
+
+              <Image style={styles.logoEmpResult} source={{ uri: selectedLogo !== null ? selectedLogo : './assets/icon.png' }} />
+            </View>
+            <Text style={styles.txtInfo}>Empresa: </Text>
+            <Text style={styles.txtInfoDados}>{selectedEmp}</Text>
+            <Text style={styles.txtInfo}>Código: </Text>
+            <Text style={styles.txtInfoDados}>{selectedTik}</Text>
+            <Text style={styles.txtInfo}>Segmento: </Text>
+            <Text style={styles.txtInfoDados}>{selectedSeg}</Text>
+            <Text style={styles.txtInfo}>Contação Intraday: </Text>
+            <Text style={styles.txtInfoDados}>R$: {selectedCot}</Text>
+            <Text style={styles.txtInfo}>Valuation Intrínseco: </Text>
+            <Text style={styles.txtInfoDados}>R$: {selectedIntrisseco}</Text>
+            <Text style={styles.txtInfo}>Valuation Fluxo de Caixa Descontado (FCD): </Text>
+            <Text style={styles.txtInfoDados}>R$: {selectedDamodaran}</Text>
+            <Text style={styles.txtInfo}>Cotação X Intrínseco: </Text>
+            <Text style={styles.txtInfoDados}>{selectedDifPercIntrisseco} %</Text>
+            <Text style={styles.txtInfo}>Cotação X FCD: </Text>
+            <Text style={styles.txtInfoDados}>{selectedDifPercDamodaran} %</Text>
+            <Text style={styles.txtInfo}>Média Intrínseco X FCD: </Text>
+            <Text style={styles.txtInfoDados}>{selectedMediaValution} %</Text>
+            <Text style={styles.txtInfo}>Cotação X Média: </Text>
+            <Text style={styles.txtInfoDados}>{selectedRiscoMedia} %</Text>
+
+          </View>
+          <View style={styles.containerBtnFechar}>
+            <TouchableOpacity style={styles.botaoModal} onPress={() => {
+              setModalVisible(false);
+              }}>
+              <Text style={styles.btnTexto}>Fechar!</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </Modal>
+
+
+
       <View style={styles.containerPesquisaEmpresa}>
         <TextInput marginBottom={10} onChangeText={(text) => findEmpresa(text)}
           leftIcon="search"
@@ -148,18 +211,8 @@ function Principal() {
         <View style={styles.containerPesquisaEmpresaResultado}>
           <FlatList data={filteredEmpresas} renderItem={renderItem} onPress={() => obterItem(item)} keyExtractor={item => item.key} extraData={selectedTik} />
         </View>
-        <Image style={styles.logoEmp} source={{ uri: selectedLogo !== null ? selectedLogo : './assets/icon.png'}}/>
-        <Text>{selectedLogo}</Text>
-        <Text>{selectedEmp}</Text>
-        <Text>{selectedTik}</Text>
-        <Text>{selectedSeg}</Text>
-        <Text>{selectedCot}</Text>
-        <Text>{selectedIntrisseco}</Text>
-        <Text>{selectedDamodaran}</Text>
-        <Text>{selectedDifPercIntrisseco}</Text>
-        <Text>{selectedDifPercDamodaran}</Text>
-        <Text>{selectedMediaValution}</Text>
-        <Text>{selectedRiscoMedia}</Text>
+
+
 
 
         <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate('login')}>
@@ -171,8 +224,11 @@ function Principal() {
   );
 }
 
+
 const Stack = createStackNavigator();
 export default class App extends React.Component {
+
+
   render() {
     return (
       <NavigationContainer >
@@ -251,6 +307,69 @@ const styles = StyleSheet.create({
     width: 46,
     height: 38,
 
+  },
+
+  containerPesquisaEmpresaResultadoShow: {
+    margin: 5,
+    marginTop: 20,
+    height: '100%',
+    backgroundColor: "white",
+    borderTopEndRadius: 20,
+    borderTopStartRadius: 20,
+
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+
+  },
+  containerLogo: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    alignSelf: 'flex-end'
+  },
+
+  logoEmpResult: {
+    width: 86,
+    height: 86,
+
+
+  },
+  containerBtnFechar: {
+
+    flex: 1,
+    alignSelf: 'center',
+    padding: 60
+
+
+
+  },
+  botaoModal: {
+    width: 240,
+    height: 42,
+    backgroundColor: "#00913D",
+    borderRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 5
+
+  },
+
+  txtInfo: {
+    fontSize: 15,
+    padding: 5,
+    fontWeight: 'bold'
+
+  },
+  txtInfoDados: {
+    fontSize: 12,
+    marginLeft: 15
   },
 
 })
